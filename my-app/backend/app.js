f420f65f02adf6abe854c0f212f1ad1e3747c19e
BenjaminLeclerc1 <b.leclercbenjamin@gmail.com>
2024-10-30 11:55:23 +0100

    Add frontend

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -1,24 +1,22 @@
-require('dotenv').config(); // Charge les variables d'environnement
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
 const fileRoutes = require('./routes/files');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/files', fileRoutes);
 
-// Test de connexion (Page d'accueil)
 app.get('/', (req, res) => {
     res.send('Hello, Dockerized Node.js App with MongoDB!');
 });
 
-// Variables d'environnement
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://mongodb:27017/mydatabase";
 
-// Connexion à MongoDB
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -30,7 +28,6 @@ mongoose.connect(MONGO_URI, {
   console.error("MongoDB connection error:", error);
 });
 
-// Démarrage du serveur
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });

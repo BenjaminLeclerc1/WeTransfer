b579e874fb75468a44e0405d2fa4ef4244007b35
DOUMBIA Siaka Koudou <127096342+Doudou210@users.noreply.github.com>
2024-10-30 14:36:13 +0100

    Update app.js

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:8080', // Remplace par l'origine du frontend
+  origin: 'http://localhost:8080',
 }));
 
 app.use(express.json());
